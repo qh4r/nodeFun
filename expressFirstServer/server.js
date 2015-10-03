e90f6f9797919b0069850bf4dd01782d7d1a38ba
Rafał Kucharski <r.kucharski@lgbs.pl>
2015-10-03 15:12:09 +0200

    obsluga modulow express

--- expressFirstServer/server.js
+++ expressFirstServer/server.js
@@ -1,37 +1,55 @@
 var express = require('express'),
+    methodOverride = require('method-override'),
     app = express(),
     port = process.env['PORT'] || 3000;
 
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function (req, res) {
     res.setHeader("Content-Type", "text/html");
-    res.send("<html><head><title>home</title></head><body><h1>Start</h1><a href='/adventure'>Go to Adventure</a></body></html>")
+    res.send("<html>" +
+        "<head>" +
+        "<title>home</title>" +
+        "</head>" +
+        "<body>" +
+        "<h1>Start</h1>" +
+        "<img src='img/rafalAsia.png'/>"
+        + "<div><a href='/adventure'>Go to Adventure</a></div>" +
+        "</body>" +
+        "</html>");
 })
 
 app.get("/adventure", function (req, res) {
     res.setHeader("Content-Type", 'text/html')
-    res.send("<html><head><title>Adventure</title></head><body><h1>Are ye bravez?</h1><form action='adventure'" +
+    res.send("<html><head><title>Adventure</title></head><body><h1>Are ye bravez?</h1><form action='/adventure'" +
         " method='POST'><button type='submit'>Yes I am bravez</button></form></body></html>")
 })
 
 app.post("/adventure", function (req, res) {
     res.setHeader("Content-Type", 'text/html')
-    res.send("<html><head><title>Adventures - NodeSlash</title></head><body><h1>Thou hast bravez.</h1><form action='adventure'" +
-        " method='POST'><button type='submit'>Again!</button></form><p>You have found some <a href='/loot/1'>loot.</a></p></body></html>")
+    res.send('<html><head><title>Adventures - NodeSlash</title></head><body><h1>Thou hast bravez.</h1><form action="/adventure"' +
+        ' method="POST"><button type="submit">Again!</button></form>' +
+        '<form action="/adventure/42?_method=PUT" method="POST"><input type="hidden" name="_method" value="PUT"><button type="submit">Cheat!</button></form>' +
+        '<p>You have found some <a href="/loot/1">loot.</a></p></body></html>')
+    console.log("adventure");
 })
 
-function updateAdventure(req,res) {
+function updateAdventure(req, res) {
     res.setHeader("Content-Type", 'text/html')
     res.send("<html><head><title>Adventures - NodeSlash</title></head><body><h1>It's a secret to everybody.</h1></body></html>")
+    console.log("updateAdventure");
+
 }
 
-function showLoot(req,res) {
+function showLoot(req, res) {
     var id = req.params.id
 
     res.setHeader("Content-Type", 'text/html')
     res.send("<html><head><title>Adventures - NodeSlash</title></head><body><h1>Ogre-slaying knife</h1><p>It has +9 against ogres. It was id #" + id + "</p></body></html>")
 }
 
-app.put('/adventures/:id', updateAdventure);
+app.put('/adventure/:id', updateAdventure);
 
 app.get('/loot/:id', showLoot);
 
