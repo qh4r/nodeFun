bf26258de4df9948bc048093fc67bfc2c774b190
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-02-16 23:59:28 +0100

    bytewiser fix part 1

--- nodeschool/bytewiser/p4.js
+++ nodeschool/bytewiser/p4.js
@@ -2,13 +2,10 @@ var lineBreak = '\n'.charCodeAt(0);
 var fs = require('fs');
 fs.readFile(process.argv[2], function(err, buffer){
 	var i = 0;
-	dump = new Buffer('');
 	while(buffer.length) {
-		if(buffer[i] !== lineBreak){
-			dump.write(''+buffer[i]);
-		} else {
-			console.log(dump.toString());
-			dump = new Buffer('');
+		if(buffer[i] === lineBreak || i > buffer.length){
+			var dump = buffer.slice(0,i);
+			console.log(dump);
 			buffer = buffer.slice(i+1);
 			i = 0;
 		}
